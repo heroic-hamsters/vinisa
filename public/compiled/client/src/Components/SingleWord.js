dad2e12a3f2c3f5238e7f25c09f1b732e4444b59
Vivian Zhang <vivian@Vivians-MacBook-Pro.local>
2017-01-02 11:21:23 -0800

    Delete babel compiled files

--- public/compiled/client/src/Components/SingleWord.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJTaW5nbGVXb3JkLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
