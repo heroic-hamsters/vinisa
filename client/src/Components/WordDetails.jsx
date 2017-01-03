686c250559cd117ccbee86f51cfc7986a83a5ef9
Vivian Zhang <vivian@Vivians-MacBook-Pro.local>
2017-01-03 15:51:05 -0800

    Set up audio drag or uploads from local and play an audio file on wordDetails page

--- client/src/Components/WordDetails.jsx
+++ client/src/Components/WordDetails.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+var Dropzone = require('react-dropzone');
 
 export default class WordDetails extends React.Component {
+  // play an audio clip
+  onAudioPlay() {
+    var audio = new Audio('https://s3.amazonaws.com/translate-hamster/audio/bottle1.m4a');
+    audio.play();
+  }
+  // upload an audio clip
+  onDrop(acceptedFiles, rejectedFiles) {
+    console.log('acceptedFiles', acceptedFiles);
+    console.log('rejectedFiles', rejectedFiles);
+  }
+
   render() {
     return (
       <div>
-        <h1>this.props.params.word</h1>
+        <h1>{this.props.params.word}</h1>
         <div>水瓶</div>
         <div>shui ping</div>
+        <div>Can you pass me that bottle?</div>
+        <div>可以递给我那个水瓶吗？</div>
+        <button onClick={this.onAudioPlay}>Play</button>
         <br/>
-        <button>Upload sample sentences here</button>
+        <button onClick={this.onAudioUpload}>Upload sentence here</button>
+        <Dropzone onDrop={this.onDrop}>
+          <div>Upload or drag an audio file here</div>
+        </Dropzone>
       </div>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import Home from './Components/Home.jsx';
 import Library from './Components/Library.jsx';
 import SearchResults from './Components/SearchResults.jsx';
 import WordDetails from './Components/WordDetails.jsx';
-import data from '../../data.js';
+import data from '../../data/sentences.js';
 
 ReactDOM.render((
   <Router history={browserHistory}>
