ff58d3844e94430e0d1bc65693990b0f388a31d8
Samuel He <samuelhe83@gmail.com>
2017-01-04 12:15:56 -0800

    Work on auth

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import { Link } from 'react-router';
 import { observer } from 'mobx-react';
 import { browserHistory } from 'react-router';
+import auth from '../auth.js';
 
 @observer
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.store = this.props.route.store;
+    this.state = {
+      error: false
+    }
   }
 
   handleSubmit(e) {
     e.preventDefault();
     this.store.username = e.target.username.value;
-    browserHistory.push('/home');
+    this.store.password = e.target.password.value;
+    auth.login(this.store.username, this.store.password, (loggedIn) => {
+      if (!loggedIn) {
+        return this.setState({error: true});
+      }
+      browserHistory.push('/home');
+    });
   }
 
   render() {
@@ -24,6 +34,7 @@ export default class Login extends React.Component {
           Username: <input type="text" name="username" />
           Password: <input type="password" name="password" />
           <input type="submit" />
+          {this.state.error && (<p>Login Failed</p>)}
         </form>
       </div>
     );
