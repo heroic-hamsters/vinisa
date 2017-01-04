c0a0debbe130e0eee199a4d531924f4f712fe8ff
Samuel He <samuelhe83@gmail.com>
2017-01-04 12:19:46 -0800

    Add password column

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,7 +2,7 @@ const db = require('../dbconfig.js');
 
 const User = db.Model.extend({
   tableName: 'users',
-  //Explicitly stating what
+  hidden: ['password'],
   sentences: () => this.belongsToMany(Sentence, 'user_sentences', 'sentence_id', 'user_id'),
   words: () => this.belongsToMany(Word, 'user_words', 'word_id', 'user_id'),
   createdSentence: () => this.hasOne(Sentence),
