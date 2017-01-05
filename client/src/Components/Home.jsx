b73265fa25761b7a05f9be691db82dcb51e88d97
Nicholas Wang <nickwang55@gmail.com>
2017-01-04 16:39:24 -0800

    Implement photoupload and vision api call

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,17 +1,84 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import Dropzone from 'react-dropzone';
+import $ from 'jquery';
 
 @observer
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.store = this.props.route.store;
+
+    this.state = {
+      imgsrc: ''
+    };
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    var file = acceptedFiles[0];
+
+    // Have to turn the uploaded picture into base64 to work with Google's API
+    var reader = new FileReader();
+    reader.onloadend = this.processFile;
+    reader.readAsDataURL(file);
+
+    this.setState({
+      imgsrc: file.preview
+    });
+  }
+
+  // POST base64 picture to Vision API with label detection parameter
+  processFile(e) {
+    var content = e.target.result.replace('data:image/jpeg;base64,', '');
+
+    var request = {
+      requests: [{
+        image: {
+          content: content
+        },
+        features: [{
+          type: 'LABEL_DETECTION'
+        }]
+      }]
+    };
+
+    $.post({
+      url: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyApUQ_2lRHs5z1DYV0skVnNgY_FXVVH6bI',
+      data: JSON.stringify(request),
+      contentType: 'application/json'
+    }).done(function(data) {
+      // Create a list using jQuery with the labels of the picture
+      var responseArray = data.responses[0].labelAnnotations;
+      var wList = $('ul.wordList');
+
+      wList.empty();
+      responseArray.forEach( response => {
+        var li = $('<li />').text(response.description).appendTo(wList);
+      });
+    });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    var chosenWord = e.target.innerHTML;
+    console.log(chosenWord);
+    // set word in the mobx store, redirect to word details page
   }
 
   render() {
     return (
       <div>
-        <button>Upload or drag a photo here</button>
+        <Dropzone style={{'width': '400px', 'height': '150px', 'border': '0.5px dashed'}} onDrop={this.onDrop.bind(this)}>
+          <div style={{'textAlign': 'center', 'marginTop': '15%'}}>Drag and drop a photo here or click to upload.</div>
+        </Dropzone>
+
+        {this.state.imgsrc === '' ? null : <img style={{'height': '300px'}} src={this.state.imgsrc} />}
+
+        <div className="wordListBox" onClick={this.handleClick.bind(this)}>
+          <ul className="wordList">
+          </ul>
+        </div>
+
       </div>
     );
   }
