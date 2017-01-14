8cde7d2c4e35c7d61851ced9ca746db1daf188f7
Nick <Nick@Nicks-MacBook-Pro.local>
2017-01-13 20:35:58 -0800

    Fix rebase issues

--- client/src/Components/WordDetails.jsx
+++ client/src/Components/WordDetails.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import Config from '../../env/config.js';
 import Dropzone from 'react-dropzone';
-<<<<<<< HEAD
-=======
-import ajax from '../lib/ajax.js';
 // import AudioRecorder from '../lib/audio-recorder';
->>>>>>> Temporarily hold off modularizing audio recording
 import MediaStreamRecorder from 'msr';
 import helpers from '../helpers.js';
 import ajax from '../lib/ajax.js';
@@ -128,10 +124,7 @@ export default class WordDetails extends React.Component {
       // url.lastModifiedDate = new Date();
       // url.name = link.download;
       this.onDrop(blob);
-<<<<<<< HEAD
-=======
 
->>>>>>> Temporarily hold off modularizing audio recording
       var file = new File([blob], tempFileName);
       this.store.audioFile = file;
       console.log('file:', file);
@@ -189,9 +182,9 @@ export default class WordDetails extends React.Component {
          <Dropzone className="audio-drop" onDrop={this.onDrop.bind(this)}>
            <div className="audio-drop-text">Upload or drag an audio file here</div>
          </Dropzone>
-            
+
           <h1>{this.store.word} {this.store.translatedWord}</h1>
-          <button onClick={this.handleClick.bind(this)}>Hear translated audio</button>
+          <button onClick={this.handleListenClick.bind(this)}>Hear translated audio</button>
           <br/>
           <br/>
           <div className="record-stop-button">
