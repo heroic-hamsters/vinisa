fea46dd2e34803e87eb51d7c653147cb341efa13
Nick <Nick@Nicks-MacBook-Pro.local>
2017-01-14 17:58:35 -0800

    Implement base functionality of changing target language

--- client/src/Components/Settings.jsx
+++ client/src/Components/Settings.jsx
@@ -10,12 +10,50 @@ export default class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.store = this.props.route.store;
+
+    this.state = {
+      error: false
+    }
   }
+
+  componentWillMount() {
+    ajax.getLanguages(function(data) {
+      data.forEach( lang => {
+
+        $('#learnLanguage').append($('<option>', {
+          value: lang.name,
+          text: lang.name
+        }));
+
+      });
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    ajax.addLanguage(e.target.language.value);
+    // console.log(e.target.language.value);
+  }
+
   render() {
     return (
-      <div>
-        Settings
-      </div>
-    )
+      <div className="login-signup-box">
+        <h1>Settings</h1>
+
+        <form className="login-signup-form" onSubmit={ this.handleSubmit.bind(this) } >
+          <div>
+            Language You'd Like to Learn:
+            <select id="learnLanguage" name="language">
+              <option></option>
+            </select>
+          </div>
+
+          <div><input id="general-button" type="submit" /></div>
+
+          {this.state.error && (<p>Signup Failed</p>)}
+
+        </form>
+
+      </div>    )
   }
 }
