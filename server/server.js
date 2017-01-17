867cc0d8d67a8afae4b405c0f4c12c9afd9cd321
Nick <Nick@Nicks-MacBook-Pro.local>
2017-01-16 20:50:52 -0800

    Fix typo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const db = require('./db/dbconfig');
 var handler = require('./request-handler');
 var session = require('express-session');
 var s3Handler = require('./s3handler');
